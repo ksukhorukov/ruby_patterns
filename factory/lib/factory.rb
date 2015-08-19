b819a1fa616649ac3e1a310d0dd4b8a4d2a34678
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-08-19 22:53:35 +0300

    Paremeterazed factory method pattern

--- factory/lib/factory.rb
+++ factory/lib/factory.rb
@@ -1,24 +1,48 @@
 class Party
   attr_accessor :members
 
-  def initialize(number)
+  def initialize(number, occupation)
     @members = []
-    number.times { members << create }
+    number.times { members << create(occupation) }
   end
 
 end
 
-class WarriorParty < Party 
-  def create
-    Warrior.new
+class PartyFactory < Party 
+  def create(occupation)
+    if [:warrior, :mage].include?(occupation)
+      self.class.const_get(occupation.to_s.capitalize).new #metaprogramming
+    else
+      raise "Occupation unknown"
+    end
   end
 end
 
-class MageParty < Party 
-  def create
-    Mage.new
-  end
-end
+#version 2
+# class PartyFactory < Party 
+#   def create(occupation)
+#     if occupation == :warrior
+#       Warrior.new 
+#     elsif occupation == :mage 
+#       Mage.new
+#     else
+#       raise "Unknown hero type"
+#     end 
+#   end
+# end
+
+#version 1
+# class WarriorParty < Party 
+#   def create
+#     Warrior.new
+#   end
+# end
+
+# class MageParty < Party 
+#   def create
+#     Mage.new
+#   end
+# end
 
 
 
