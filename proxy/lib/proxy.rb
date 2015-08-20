109d768ce88d1226431d58a35b326dbcac99595d
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-08-20 12:06:13 +0300

    Auth with proxy

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,13 +1,23 @@
 require 'forwardable'
 
+class Hero
+
+  attr_accessor :keywords
+
+  def initialize
+    @keywords = []
+  end
+  
+end
+
 class ComputerProxy
   
   extend Forwardable
 
-  def_delegators :@real_object, :add, :execute
-
-  def initialize(real_object)
+  def_delegators :@real_object, :add
+  def initialize(real_object, hero)
     @real_object = real_object
+    @hero = hero
   end 
 
   #solution 1
@@ -23,6 +33,15 @@ class ComputerProxy
   # def method_missing(name, *args) 
   #   @real_object.send(name, *args)
   # end
+
+  def execute
+    check_access
+    @real_object.execute
+  end
+
+  def check_access
+    raise 'You have no access' unless @hero.keywords.include?(:computer)
+  end
 
 end
 
