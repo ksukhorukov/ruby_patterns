4bb4a640cfbd043305e48c2f47db476f37464ddb
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-08-20 12:00:55 +0300

    use forwardable

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,16 +1,28 @@
+require 'forwardable'
+
 class ComputerProxy
   
+  extend Forwardable
+
+  def_delegators :@real_object, :add, :execute
+
   def initialize(real_object)
     @real_object = real_object
   end 
 
-  def add(command)
-    @real_object.add(command)
-  end
+  #solution 1
+  # def add(command)
+  #   @real_object.add(command)
+  # end
 
-  def execute
-    @real_object.execute
-  end
+  # def execute
+  #   @real_object.execute
+  # end
+
+  #solution 2
+  # def method_missing(name, *args) 
+  #   @real_object.send(name, *args)
+  # end
 
 end
 
